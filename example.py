d1f8ce7ba2ad2548724faa29aedcb646b4f1643a
Karl Hakkarainen <kh@queenlake.com>
2017-02-10 15:05:40 -0500

    validated example

--- example.py
+++ example.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python
-
-# -*- coding: utf-8 -*- 
-# Import extensible library for opening URLs
+# -*- coding: utf-8 -*- # Import extensible library for opening URLs
 import urllib2, urllib
-
 # Segment for translation
-contentForTx  = "Acme Corp has the largest selection of dynamite and anvils on the market."
-
+contentForTx = "Acme Corp has the largest selection of dynamite and anvils on the market."
 # The hostname of the project that contains the TM and OTX account
-virtualHostName  = "es.acme.com"
-
+virtualHostName = "es-otx.onelink-poc.com"
 # The hostname of the server the project is contained on
-physicalHostName = "stagex1.onelink-translations.com"
-
+physicalHostName = "es-otx.onelink-poc.com"
 # Set the parameters of the request, encode with url encoding.
 params = urllib.urlencode({'otx_mimetype': 'text/html',
-                           'otx_account' : 'acme,acmePassW0rd',
-                           'otx_service' : 'tx',
-                           'otx_content' : contentForTx})
+ 'otx_account' : 'otx,otxpass', 'otx_service' : 'smt', 'otx_content' : contentForTx})
 # Set the host header
 headers = {"Host": virtualHostName}
-
 # Make the request object passing in the parameters and headers
-req = urllib2.Request(b:: Content for translation    : Acme Corp has the largest selection of dynamite and anvils on the market.
-:: Received translated content: Acme Corp tiene la mayor selecciC3n de dinamita y yunques en el mercado.
+req = urllib2.Request("https://"+physicalHostName+"/OneLinkOTX/", params, headers)
+# Execute the request
+response = urllib2.urlopen(req)
+# Read the response
+htmlData = response.read()
+print ":: Content for translation : "+contentForTx
+print ":: Received translated content: "+htmlData
